7a11c40eeb2778f529e0bd5d8cc87d60cc374c62
Emanuele <emanuelecz06@gmail.com>
2025-11-17 11:57:56 +0100

    fixed some errors

--- backend/middlewares/isAuth.js
+++ backend/middlewares/isAuth.js
@@ -1,20 +1,22 @@
-import jwt from 'jsonwebtoken';
-const SECRET_KEY = process.env.JWT_SECRET
 
+import jwt from "jsonwebtoken";
 export const isAuth = (req, res, next) => {
-    try{
-        const authHeader = req.headers.authorization;
-        if(!authHeader || !authHeader.startsWith("Bearer")){
-            return res.status(401).json({message: "Not authenticated"});
-        }
-
-        const token = authHeader.split(" ")[1];
-        const decoded = jwt.verify(token, SECRET_KEY);
-        req.user = decoded;
-        next();
-    } catch (error) {
-        return res.status(401).json({message: "Invalid or expired token"});
-    }
+  try {
     
 
-}
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith("Bearer")) {
+      return res.status(401).json({ message: "Not authenticated" });
+    }
+
+    const token = authHeader.split(" ")[1];
+    
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);;
+
+    req.user = decoded;
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "Invalid or expired token" });
+  }
+};
